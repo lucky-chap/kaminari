de91096a59d95bc5eeab40d14b68f6185e7f6fd9
Virgil <hunchodotdev@gmail.com>
2025-01-16 08:26:09 +0000

    fix wrong type import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 
-import type Metadata from "next";
+import type { Metadata } from 'next/types'
 import localFont from "next/font/local";
 
 import { Provider } from "@/components/provider";
