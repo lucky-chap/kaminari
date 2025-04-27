30f0c2ac975adde87564be70ac03dc116c9f53a3
Huncho <hunchodotdev@gmail.com>
2025-04-27 12:56:32 +0000

    Update tailwindcss to v4. Update Next.js to 15.3.1

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,29 +5,29 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-400 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-zinc-800",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-zinc-400 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-zinc-800",
   {
     variants: {
       variant: {
         default:
-          "bg-zinc-900 text-zinc-50 shadow hover:bg-zinc-900/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90",
+          "bg-zinc-900 text-zinc-50 shadow-sm hover:bg-zinc-900/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90",
         destructive:
-          "bg-red-500 text-zinc-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-red-50 dark:hover:bg-red-900/90",
+          "bg-red-500 text-zinc-50 shadow-xs hover:bg-red-500/90 dark:bg-red-900 dark:text-red-50 dark:hover:bg-red-900/90",
         outline:
-          "border border-zinc-200 bg-white shadow-sm hover:bg-zinc-100 hover:text-zinc-900 dark:border-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
+          "border border-zinc-200 bg-white shadow-xs hover:bg-zinc-100 hover:text-zinc-900 dark:border-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
         secondary:
-          "bg-zinc-100 text-zinc-900 shadow-sm hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80",
+          "bg-zinc-100 text-zinc-900 shadow-xs hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80",
         ghost:
           "hover:bg-zinc-100 hover:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
         link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
         brand:
-          "inline-block text-base transition-all duration-100 ease-linear font-medium hover:bg-black/90 group rounded bg-black px-7 py-4 text-center text-white dark:bg-white dark:text-black dark:hover:bg-white/90",
+          "inline-block text-base transition-all duration-100 ease-linear font-medium hover:bg-black/90 group rounded-sm bg-black px-7 py-4 text-center text-white dark:bg-white dark:text-black dark:hover:bg-white/90",
       },
       size: {
         default: "px-4 py-3",
-        sm: "py-2 rounded px-3 text-xs",
-        lg: "py-3 rounded px-8",
-        xl: "py-4 rounded px-8",
+        sm: "py-2 rounded-sm px-3 text-xs",
+        lg: "py-3 rounded-sm px-8",
+        xl: "py-4 rounded-sm px-8",
         icon: "h-9 w-9",
       },
     },
